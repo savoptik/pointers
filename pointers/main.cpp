c110d62676f593352c02a4682c36e17c15343cc1
savoptik <sav70792@gmail.com>
2018-10-15 19:05:01 +0300

    Initial Commit

---
+++ pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointers
+//
+//  Created by Артём Семёнов on 15/10/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
